bb7077a13647a8c86dd58724af9ea705762fdbfe
w.antar <w.antar@romulus.live>
2025-10-05 15:11:19 +0100

    initial commit

---
+++ src/main/java/com/irap/irap_backend/IrapBackendApplication.java
@@ -0,0 +1,13 @@
+package com.irap.irap_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IrapBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IrapBackendApplication.class, args);
+	}
+
+}
